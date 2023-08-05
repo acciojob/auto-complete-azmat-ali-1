577a002bed33c7a037ec84335169c83174e03670
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-08-06 02:18:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
 
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+  const [fruitItem, setFruitItem] = useState(fruits);
+
+  function handleSearchInput(e) {
+    let filteredFruit = fruits.filter(
+      (fruit) => fruit.indexOf(e.target.value) > -1
+    );
+    setFruitItem(filteredFruit);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type="text" onChange={handleSearchInput} />
+      <ul>
+        {fruitItem.map((fruit) => (
+          <li>{fruit}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
